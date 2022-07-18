d398d9a2dd241e6aac42034e0c597d9ca3c9a224
Confidence Madiope <confidencem97@gmail.com>
2022-07-18 16:20:53 +0200

    ADD: background image to home page

--- pages/components/MidSection.js
+++ pages/components/MidSection.js
@@ -1,18 +1,18 @@
 import React from "react";
 import family from "../../public/images/family.svg";
 import styled from "styled-components";
+import styles from '../../styles/MidSection.module.css'
 
 const MidSection = () => {
-  return <Wrapper img={family}></Wrapper>;
+  return (
+    <div className={styles.bg}>
+      <div className={styles.container}>
+        <h3 className={styles.h3}>
+          TIME TO MOVE
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default MidSection;
-
-const Wrapper = styled.div`
-  width: 280px;
-  height: 50px;
-  background-size: contain;
-  background-image: ${(props) => `url(${props.img})`};
-`;
-
-// videos @10 minutes
